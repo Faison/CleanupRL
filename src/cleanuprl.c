fda18d2956b3ab3902b448dc3abef03c2f28854f
Faison <fzutavern@my.wctc.edu>
2015-03-07 21:52:17 -0600

    Updated the menu test file to be the main file and used the dialog

--- src/cleanuprl.c
+++ src/cleanuprl.c
@@ -1,6 +1,7 @@
 #include <ncurses.h>
 #include <string.h>
 #include "menus.h"
+#include "dialog.h"
 
 int main()
 {
@@ -13,11 +14,16 @@ int main()
 	char *options[] = {"New Game", "About", "Quit"};
 	char *selection = "Main";
 
+	char *about_lines[] = {"Some text about things",
+				"And other stuff when I care about",
+				"other stuff."};
+
 	do {
 		if ( strcmp(selection, "Main") == 0 ) {
-			selection = show_menu( "CleanupRL", options, 3);
+			selection = show_menu("CleanupRL", options, 3);
 		} else if ( strcmp(selection, "About") == 0 ) {
-			selection = show_menu( "About", options, 3);
+			show_dialog("About CleanupRL", about_lines, 3);
+			selection = "Main";
 		} else if ( strcmp(selection, "New Game") == 0 ) {
 			selection = "Main";
 		}
