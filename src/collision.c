97a5f45059713a90fb711e4c5d7e756e456c1855
Faison <fzutavern@my.wctc.edu>
2015-03-12 21:29:22 -0500

    Fixed the collision bug that allowed you to walk on walls

--- src/collision.c
+++ src/collision.c
@@ -13,45 +13,41 @@ void handle_player_collisions( Player *player, House *house ) {
 	new_x = player->x + player->x_velocity;
 	new_y = player->y + player->y_velocity;
 
-	mvprintw(30, 10, "Player x: %d y: %d x_v: %d y_v: %d", player->x, player->y, player->x_velocity, player->y_velocity );
-
 	Room *room = get_room_at_point( house, new_x, new_y);
 
-	mvprintw(28, 10, "Room x: %d y: %d w: %d h: %d", room->x, room->y, room->width, room->height );
-
 	if (player->y_velocity != 0) {
-		if (new_y == room->y && room->walls & CRL_WALL_TOP) {
+		if (new_y == room->y) {
 			if (new_x == room->x && room->walls & CRL_WALL_TOP_LEFT) {
 				valid = 0;
-			} else if (new_x == room->x + room->width && room->walls & CRL_WALL_TOP_RIGHT) {
+			} else if (new_x + 1 == room->x + room->width && room->walls & CRL_WALL_TOP_RIGHT) {
 				valid = 0;
-			} else {
+			} else if (room->walls & CRL_WALL_TOP) {
 				valid = 0;
 			}
-		} else if (new_y + 1 == room->y + room->height && room->walls & CRL_WALL_BOTTOM) {
+		} else if (new_y + 1 == room->y + room->height) {
 			if (new_x == room->x && room->walls & CRL_WALL_BOTTOM_LEFT) {
 				valid = 0;
-			} else if (new_x == room->x + room->width && room->walls & CRL_WALL_BOTTOM_RIGHT) {
+			} else if (new_x + 1 == room->x + room->width && room->walls & CRL_WALL_BOTTOM_RIGHT) {
 				valid = 0;
-			} else {
+			} else if (room->walls & CRL_WALL_BOTTOM) {
 				valid = 0;
 			}
 		}
 	} else if (player->x_velocity != 0) {
-		if (new_x == room->x && room->walls & CRL_WALL_LEFT) {
+		if (new_x == room->x) {
 			if (new_y == room->y && room->walls & CRL_WALL_TOP_LEFT) {
 				valid = 0;
-			} else if (new_y == room->y + room->height && room->walls & CRL_WALL_BOTTOM_LEFT) {
+			} else if (new_y + 1 == room->y + room->height && room->walls & CRL_WALL_BOTTOM_LEFT) {
 				valid = 0;
-			} else {
+			} else if (room->walls & CRL_WALL_LEFT) {
 				valid = 0;
 			}
-		} else if (new_x + 1 == room->x + room->width && room->walls & CRL_WALL_RIGHT) {
+		} else if (new_x + 1 == room->x + room->width ) {
 			if (new_y == room->y && room->walls & CRL_WALL_TOP_RIGHT) {
 				valid = 0;
 			} else if (new_y == room->y + room->height && room->walls & CRL_WALL_BOTTOM_RIGHT) {
 				valid = 0;
-			} else {
+			} else if (room->walls & CRL_WALL_RIGHT) {
 				valid = 0;
 			}
 		}
